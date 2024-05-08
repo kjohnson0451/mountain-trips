e1360d50cb66eff2e83525dfa82c7ada3ed00904
Keith Johnson <kjohnson0451@gmail.com>
2024-05-08 14:01:17 -0400

    Made a calendar item

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -2,7 +2,29 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <h1>Calendar</h1>
+        <h1 className="mb-4">Calendar</h1>
+        <div className="card" style={{ width: "48rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Northern Long Trail Backpacking Trip</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">
+              Jul 5 - Jul 7 2024
+            </h6>
+            <h6 className="card-subtitle mb-2" style={{ color: "#198754" }}>
+              $35.50
+            </h6>
+            {/* <h6 className="card-subtitle mb-2 text-body-secondary">$35.50</h6> */}
+            <p className="card-text">
+              Enjoy a backpacking weekend among one of the more remote areas of
+              the Green Mountains! We&apos;ll be camping and hanging out on
+              Friday and Saturday evenings. During the day on Saturday and
+              Sunday, we&apos;ll be backpacking along the Long Trail at rougly
+              15 miles per day.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Book now
+            </a>
+          </div>
+        </div>
       </div>
     </main>
   )
