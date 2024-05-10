747259631bee7f133390a9eaecec0183c5dd2a0a
Keith Johnson <kjohnson0451@gmail.com>
2024-05-10 09:17:29 -0400

    use fetchTrips in calendar page

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,23 +1,22 @@
 import EventCard from "@/app/ui/calendar/event-card"
 import { fetchTrips } from "@/lib/data"
 
-export default function Calendar() {
+export default async function Calendar() {
+  const trips = await fetchTrips()
   return (
     <main>
       <div className="container">
         <h1 className="mb-4">Calendar</h1>
-        <div className="card" style={{ width: "48rem" }}>
-          <EventCard
-            title="Northern Long Trail Backpacking Trip"
-            date="Jul 5 - Jul 7 2024"
-            price={35.5}
-            description="Enjoy a backpacking weekend among one of the more remote areas of
-            the Green Mountains! We'll be camping and hanging out on
-            Friday and Saturday evenings. During the day on Saturday and
-            Sunday, we'll be backpacking along the Long Trail at rougly
-            15 miles per day."
-          />
-        </div>
+        {trips.map((trip) => (
+          <div key={trip.id} className="card" style={{ width: "48rem" }}>
+            <EventCard
+              title={trip.title}
+              date="Jul 5 - Jul 7 2024"
+              price={trip.price}
+              description={trip.description}
+            />
+          </div>
+        ))}
       </div>
     </main>
   )
