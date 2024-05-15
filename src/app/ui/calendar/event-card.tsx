24e2d5e09d075e9eda3c992ebfbee1d2c73bdc1c
Keith Johnson <kjohnson0451@gmail.com>
2024-05-15 19:16:00 -0400

    Handle price for trip

--- src/app/ui/calendar/event-card.tsx
+++ src/app/ui/calendar/event-card.tsx
@@ -3,17 +3,13 @@ import { Trip } from "@prisma/client"
 type EventCardProps = Trip
 
 export default function EventCard({ trip }: { trip: EventCardProps }) {
-  const formattedPrice = (35.5).toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-  })
   return (
     <div className="card" style={{ width: "48rem" }}>
       <div className="card-body">
         <h5 className="card-title">{trip.title}</h5>
         <h6 className="card-subtitle mb-2 text-body-secondary">{"What"}</h6>
         <h6 className="card-subtitle mb-2" style={{ color: "#198754" }}>
-          {formattedPrice}
+          ${trip.price.toFixed(2)}
         </h6>
         <p className="card-text text-truncate">{trip.description}</p>
         <a href="#" className="btn btn-primary">
