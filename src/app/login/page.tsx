dc0c6c70d50a55a362278b2d6912b1f3670d5221
Keith Johnson <kjohnson0451@gmail.com>
2024-06-21 07:36:17 -0400

    Wrapped login form in suspense boundary

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { LoginForm } from "./form"
 
 export default function LoginPage() {
@@ -6,7 +7,9 @@ export default function LoginPage() {
       <section className="bg-ct-blue-600 min-h-screen pt-20">
         <div className="container mx-auto px-6 py-12 h-full flex justify-center items-center">
           <div className="md:w-8/12 lg:w-5/12 bg-white px-8 py-10">
-            <LoginForm />
+            <Suspense>
+              <LoginForm />
+            </Suspense>
           </div>
         </div>
       </section>
